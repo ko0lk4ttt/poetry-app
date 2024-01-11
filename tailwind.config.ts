9ec914e584e5f16193fe67710a60d4da3c2bbaa4
Richard Aruofor <richard.aruofor@giffgaff.co.uk>
2024-01-11 11:16:54 +0000

    react-daisyui setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,8 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,6 +17,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')  ],
 }
 export default config
